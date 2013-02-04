6cabde4888f88b01cb1795d8f4f38afaeec1a287
markwilliams <markwilliams@markw-macbook-pro.local>
2013-02-03 22:43:44 -0700

    User-specific variables

--- my_vars.rb
+++ my_vars.rb
@@ -14,4 +14,4 @@ $my_base_url		        = "https://rally1.rallydev.com/slm"
 # However, when creating/updating only one or two users, the up-front cost of caching is probably more expensive
 # than the time saved, so setting this flag to false probably makes sense when creating/updating small
 # numbers of users
-$enable_cache                       = true
+$enable_cache           = true
