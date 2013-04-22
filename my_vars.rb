4277c6625708b5e1a7869afcbf58b36e8608c8e3
U-markw-mbp-PC\markwilliams <mdwilliams@rallydev.com>
2013-04-21 19:56:31 -0600

    User-specific environment variables

--- my_vars.rb
+++ my_vars.rb
@@ -3,18 +3,14 @@ $my_password		        = 'topsecret'
 $my_base_url		        = "https://rally1.rallydev.com/slm"
 
 #API Version
-$wsapi_version          = "1.41"
+$wsapi_version                  = "1.41"
 
-# The $enable_cache parameter applies only when using the user_permissions_loader.rb script
+# The $enable_user_cache parameter applies only when using the user_permissions_loader.rb script
 
-# Note: When creating many users, pre-fetching UserPermissions, Workspaces and Projects
-# can radically improve performance since it also allows for
-# a memory cache of existing Workspace/Projects and Workspace/Project permissions in Rally.
-#This avoids the need to go back to Rally with a query in order to check for Workspace/Project existence and
-# if a Permission update represents a change with respect to what's already there.
-# Doing this in memory makes the code run much faster
+# Note: When creating or updating many users, pre-fetching UserPermissions
+# can improve performance
 
 # However, when creating/updating only one or two users, the up-front cost of caching is probably more expensive
 # than the time saved, so setting this flag to false probably makes sense when creating/updating small
 # numbers of users
-$enable_cache           = true
+$enable_user_cache              = false
