5f66d9829e59d7afb134ece786d6711d7a790b83
markwilliams <markwilliams@markw-macbook-pro.local>
2013-09-22 17:47:27 -0600

    Generates a template of users and Workspace / Project permissions for a list of users specified in an input text file. Output is used as input to the user_permissions_loader.rb

--- user_permissions_template_generator.rb
+++ user_permissions_template_generator.rb
@@ -1,4 +1,22 @@
-# Copyright 2002-2013 Rally Software Development Corp. All Rights Reserved.
+# Copyright (c) 2013 Rally Software Development
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 #include for rally json library gem
 require 'rally_api'
@@ -10,9 +28,9 @@ require './multi_io.rb'
 
 #Setting custom headers
 $headers                        = RallyAPI::CustomHttpHeader.new()
-$headers.name                   = "Ruby User Permissions Template Generator"
+$headers.name                   = "Ruby User Management Tool 2::Ruby User Permissions Template Generator"
 $headers.vendor                 = "Rally Labs"
-$headers.version                = "0.20"
+$headers.version                = "0.50"
 
 #API Version
 $wsapi_version                   = "1.43"
