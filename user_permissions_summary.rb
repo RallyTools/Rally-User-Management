f21d0b3352037f97384a6861f0232091ba1b53a3
markwilliams <markwilliams@markw-macbook-pro.local>
2013-09-22 17:46:49 -0600

    Script to output summary of all User Workspace/Project Permissions within Rally Subscription.

--- user_permissions_summary.rb
+++ user_permissions_summary.rb
@@ -1,12 +1,32 @@
+# Copyright (c) 2013 Rally Software Development
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 #include for rally json library gem
 require 'rally_api'
 require 'csv'
 
 #Setting custom headers
 $headers = RallyAPI::CustomHttpHeader.new()
-$headers.name           = "Ruby User Permissions Summary Report"
+$headers.name           = "Ruby User Management Tool 2::User Permissions Loader::User Permissions Summary Report"
 $headers.vendor         = "Rally Labs"
-$headers.version        = "0.20"
+$headers.version        = "0.50"
 
 #API Version
 $wsapi_version          = "1.43"
@@ -36,7 +56,7 @@ $initial_fetch            = "UserName,FirstName,LastName,DisplayName"
 $standard_detail_fetch    = "UserName,FirstName,LastName,DisplayName,UserPermissions,Name,Role,Workspace,ObjectID,State,Project,ObjectID,State,TeamMemberships"
 $extended_detail_fetch    = "UserName,FirstName,LastName,DisplayName,UserPermissions,Name,Role,Workspace,ObjectID,State,Project,ObjectID,State,TeamMemberships,Disabled,NetworkID,Role,CostCenter,Department,OfficeLocation"
 
-$enabled_only_filter = "(Disabled = \"False\")"
+$enabled_only_filter      = "(Disabled = \"False\")"
 
 if $summary_mode == :extended then
   $summarize_enabled_only = false
