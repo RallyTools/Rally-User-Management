d627d8fec7304a91e2b60f23b7971e149547f8ed
markwilliams <markwilliams@markw-macbook-pro.local>
2013-09-22 17:41:47 -0600

    Script to bulk update usernames and email addresses

--- change_usernames.rb
+++ change_usernames.rb
@@ -1,18 +1,36 @@
-# Copyright 2002-2011 Rally Software Development Corp. All Rights Reserved.
+# Copyright (c) 2013 Rally Software Development
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 require 'rally_api'
 require 'csv'
 
-$my_base_url       = "https://rally1.rallydev.com/slm"
+$my_base_url                   = "https://rally1.rallydev.com/slm"
 
-$my_username       = "user@company.com"
-$my_password       = "password"
-$wsapi_version     = "1.43"
+$my_username                   = "user@company.com"
+$my_password                   = "password"
+$wsapi_version                 = "1.43"
 
 # Mode options:
 # :usernameandemail => resets both UserName and Email to the updated value
 # :usernameonly => only resets UserName. Email address remains unchanged
-$mode              = :usernameandemail
+$user_update_mode              = :usernameandemail
 
 $users_filename = ARGV[0]
 
@@ -49,7 +67,7 @@ def update_username(header, row)
       rally_user_toupdate = rally_user.first()
       fields = {}
       fields["UserName"] = new_username
-      if $mode == :usernameandemail then
+      if $user_update_mode == :usernameandemail then
         fields["EmailAddress"] = new_username
       end
       rally_user_updated = @rally.update(:user, rally_user_toupdate.ObjectID, fields) #by ObjectID
@@ -66,10 +84,10 @@ begin
   #==================== Making a connection to Rally ====================
 
   #Setting custom headers
-  $headers                            = RallyAPI::CustomHttpHeader.new()
-  $headers.name                       = "Ruby User Management Tool 2: Change Usernames"
-  $headers.vendor                     = "Rally Labs"
-  $headers.version                    = "0.20"
+  $headers                = RallyAPI::CustomHttpHeader.new()
+  $headers.name           = "Ruby User Management Tool 2::Change Usernames"
+  $headers.vendor         = "Rally Labs"
+  $headers.version        = "0.50"
 
   config                  = {:base_url => $my_base_url}
   config[:username]       = $my_username
