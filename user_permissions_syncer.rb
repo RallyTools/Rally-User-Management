c8765cb69bdb95b81b8a1a167e9892b2bd11d92c
markwilliams <markwilliams@markw-macbook-pro.local>
2013-11-02 18:37:36 -0600

    Script to synchronize permissions and team memberships from a list of existing users in Rally to a list of target users needing updated permissions.

--- user_permissions_syncer.rb
+++ user_permissions_syncer.rb
@@ -24,7 +24,7 @@
 # Expected input files are defined as global variables below
 
 # Delimited list of user permissions:
-# $permissions_filename    = 'user_permissions_loader.txt'
+# $user_synclist_filename    = 'user_sync_list.txt'
 
 require 'rally_api'
 require 'csv'
