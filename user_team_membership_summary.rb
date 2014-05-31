51276ba338b9099ed7715658764cf73cc533eacb
markwilliams970 <mdwrally970@gmail.com>
2014-05-31 13:31:19 -0600

    Script to output a summary of all user team memberships within Rally

--- user_team_membership_summary.rb
+++ user_team_membership_summary.rb
@@ -18,109 +18,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-#!/usr/bin/ruby
-########################################################################
-util_name = "user_team_membership_summary"
-########################################################################
-
+#include for rally json library gem
 require 'rally_api'
-require './user_mgmt_version'
+require 'rally_user_management'
 require 'csv'
+require './lib/go_user_team_membership_summary.rb'
 
-$my_username                = 'user@company.com'
-$my_password                = 'password'
-$my_base_url                = "https://rally1.rallydev.com/slm"
+$output_file_arg = ARGV[0]
 
-$my_page_size               = 50
-$my_fetch                   = "true"
-$my_workspace               = "My Workspace"
-$my_project                 = "My Project"
-
-$my_output_file             = "user_team_membership_summary.txt"
-
-# Output file delimiter
-$my_delim = "\t"
-
-$output_fields              =  %w{UserID MembershipNumber TeamName}
-
-#Setting custom headers
-@user_mgmt_version          = UserManagementVersion.new()
-$headers                    = RallyAPI::CustomHttpHeader.new()
-$headers.name               = "Ruby User Management Tool 2::Ruby Team Membership Summary Report"
-$headers.vendor             = "Rally Labs"
-$headers.version            = @user_mgmt_version.revision()
-
-#API Version
-$wsapi_version              = "1.43"
-
-# Encoding
-$file_encoding              = "US-ASCII"
-
-# Load (and maybe override with) my personal/private variables from a file...
-my_vars= File.dirname(__FILE__) + "/my_vars.rb"
-if FileTest.exist?( my_vars ) then require my_vars end
-
-#==================== Making a connection to Rally ====================
-config                  = {:base_url => $my_base_url}
-config[:username]       = $my_username
-config[:password]       = $my_password
-config[:version]        = $wsapi_version
-config[:headers]        = $headers #from RallyAPI::CustomHttpHeader.new()
-
-puts "Connecting to Rally: #{$my_base_url} as #{$my_username}..."
-@rally = RallyAPI::RallyRestJson.new(config)
-
-#==================== Querying Rally ==========================
-user_query = RallyAPI::RallyQuery.new()
-user_query.type = :user
-user_query.fetch = "UserName,FirstName,LastName,DisplayName,TeamMemberships,Name,Role,Project"
-user_query.page_size = 200 #optional - default is 200
-user_query.limit = 50000 #optional - default is 99999
-user_query.order = "UserName Asc"
-
-# Query for users
-puts "Querying users..."
-
-results = @rally.find(user_query)
-
-number_users = results.total_result_count
-puts "Found #{number_users} users."
-
-# Start output of summary
-# Output CSV header
-summary_csv = CSV.open($my_output_file, "wb", {:col_sep => $my_delim, :encoding => $file_encoding})
-summary_csv << $output_fields
-
-# loop through all users and output permissions summary
-puts "Summarizing users and writing permission summary output file..."
-
-# Step thru all users
-count = 0
-results.each do | this_User |
-
-    count = count + 1
-    number_team_memberships = this_User.TeamMemberships != nil ? this_User.TeamMemberships.length : 0
-
-    if number_team_memberships > 0
-      ct = 0
-        this_User.TeamMemberships.each do |this_Team|
-          # Print user info...
-
-          output_record = []
-          output_record << this_User.UserName
-          output_record << "#%02d"%ct
-          output_record << this_Team.Name
-
-          summary_csv << output_record
-          ct = ct + 1
-        end
-    else
-      output_record = []
-      output_record << this_User.UserName
-      output_record << "#00"
-      output_record << "No Team Memberships"
-      summary_csv << output_record
-    end
+if $output_file_arg == nil
+# This is the default of the file to be used for uploading user permissions
+    $output_filename =  File.dirname(__FILE__) + "/" + 'user_team_membership_summary.txt'
+else
+    $output_filename = File.dirname(__FILE__) + "/" + ARGV[0]
 end
 
-puts "Done! User team membership summary written to #{$my_output_file}."
+begin
+  go_user_team_membership_summary($output_filename)
+end
