f61457f0613d7cb36047b872cf85bc0aa708e49b
markwilliams970 <mdwrally970@gmail.com>
2014-05-31 13:26:28 -0600

    Script to load LDAP Usernames for existing Rally Users - utility to assist Rally On-Premise Administrators.

--- ldap_username_load.rb
+++ ldap_username_load.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # Copyright (c) 2014 Rally Software Development
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,101 +18,30 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+#include for rally json library gem
 require 'rally_api'
-require './user_mgmt_version'
+require 'rally_user_management'
 require 'csv'
+require './lib/go_ldap_username_load.rb'
 
-rally_url              = "https://10.32.10.120/slm"
-rally_user             = "subadmin@company.com"
-rally_password         = "topsecret"
+$rally_url              = "http://10.32.20.131/slm"
+$rally_user             = "subadmin@company.com"
+$rally_password         = "topsecret"
 
 # Default to WSAPI 1.33 to accommodate potentially older On-Premise appliances
-rally_ws_version       = "1.33"
-filename               = 'ldap_username_load_template.csv'
+$rally_wsapi_version    = "1.33"
 
-# Encoding
-$file_encoding         = "US-ASCII"
+# MAKE NO CHANGES BELOW THIS LINE!!
+# ============================================================================
+$input_file_arg = ARGV[0]
 
-def get_rally_users()
-
-  user_query = RallyAPI::RallyQuery.new()
-  user_query.type = :user
-  user_query.fetch = "ObjectID,UserName,FirstName,LastName,OnpremLdapUsername"
-  user_query.query_string = '(OnpremLdapUsername = "")'
-  user_query.order = "UserName Asc"
-
-  users = @rally.find(user_query)
-  return users
-
+if $input_file_arg == nil
+# This is the default of the file to be used for uploading user permissions
+  $input_filename =  File.dirname(__FILE__) + "/" + 'ldap_username_load_template.csv'
+else
+  $input_filename = File.dirname(__FILE__) + "/" + ARGV[0]
 end
-#============Update user function================
-def update_user(header, row)
-  username        = row[header[0]]
-  onprem_username = row[header[1]]
-
-  user_query = RallyAPI::RallyQuery.new()
-  user_query.type = :user
-  user_query.fetch = "ObjectID,UserName,FirstName,LastName,OnpremLdapUsername"
-  user_query.query_string = "(UserName = \"" + username + "\")"
-  user_query.order = "UserName Asc"
-
-  rally_user = @rally.find(user_query)
-
-  if rally_user.total_result_count == 0
-    puts "Rally user #{username} not found"
-  else
-    begin
-      rally_user_toupdate = rally_user.first()
-      fields = {}
-      fields["OnpremLdapUsername"] = onprem_username
-      rally_user_updated = @rally.update(:user, rally_user_toupdate.ObjectID, fields) #by ObjectID
-      puts "Rally user #{username} updated successfully - onprem username set to #{rally_user_updated["OnpremLdapUsername"]}"
-
-    rescue => ex
-      puts " Rally user #{username} not updated due to error"
-      puts ex
-    end
-  end
-end
-#=================End of function=============
 
 begin
-
-  #==================== Making a connection to Rally ====================
-
-  #Setting custom headers
-  @user_mgmt_version      = UserManagementVersion.new()
-  $headers = RallyAPI::CustomHttpHeader.new()
-  $headers.name           = "Ruby User Management Tool 2::Ruby LDAP User Load Script"
-  $headers.vendor         = "Rally Labs"
-  $headers.version        = @user_mgmt_version.revision()
-
-  config                  = {:base_url => rally_url}
-  config[:username]       = rally_user
-  config[:password]       = rally_password
-  config[:version]        = rally_ws_version
-  config[:headers]        = $headers #from RallyAPI::CustomHttpHeader.new()
-
-  @rally = RallyAPI::RallyRestJson.new(config)
-
-  input  = CSV.read(filename, {:encoding => $file_encoding})
-  header = input.first #ignores first line
-
-  rows   = []
-  (1...input.size).each { |i| rows << CSV::Row.new(header, input[i]) }
-
-  rows.each do |row|
-    update_user(header, row)
-  end
-
-  puts "Querying to find existing Rally Users without an OnpremLdapUsername attribute..."
-  rally_users = get_rally_users()
-  puts "Found: #{rally_users.total_result_count} that do not have an OnpremLdapUsername."
-  puts "Listing..."
-  rally_users.each do |user|
-    puts "Rally user #{user["UserName"]} does not contain a ldap onprem username value"
-  end
-rescue => ex
-  puts ex
-  puts ex.backtrace
+  go_ldap_username_load($input_filename)
 end
