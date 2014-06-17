9c0338c2d616b03dd18ad4890b017a39b59ca896
markwilliams970 <mdwrally970@gmail.com>
2014-06-17 17:01:27 -0600

    Script to synchronize permissions and team memberships from a list of existing users in Rally to a list of target users

--- user_permissions_syncer.rb
+++ user_permissions_syncer.rb
@@ -20,8 +20,9 @@
 
 #include for rally json library gem
 require 'rally_api'
-require 'rally_user_management'
 require 'csv'
+require './lib/multi_io.rb'
+require './lib/rally_user_helper.rb'
 require './lib/go_user_permissions_syncer.rb'
 
 $user_synclist_filename_arg = ARGV[0]
